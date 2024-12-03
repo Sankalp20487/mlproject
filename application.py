58d6501f0afc4907e0dd5fdc5578cacefd5d79aa
Sankalp20487 <sankalpbiswal95@gmail.com>
2024-12-02 21:37:10 -0500

    Update port number

--- application.py
+++ application.py
@@ -41,6 +41,6 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__ == "__main__":
-    app.run(port=5001)  # Change port number to 5001 or any other available port
+    app.run(host='0.0.0.0', port=8080)          # Change port number to 5001 or any other available port
      
 
